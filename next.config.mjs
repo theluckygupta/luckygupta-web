fb765042fa5126e9f2b10747fafd7a656b67e3b1
Lucky Gupta <44520571+theluckygupta@users.noreply.github.com>
2026-05-04 08:00:00 +0000

    fix config + sitemap

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */ 
 const nextConfig = {
   images: {
     domains: ['aceternity.com', 'images.unsplash.com', 'assets.aceternity.com'],
-  },};
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
