e7a5011df7a7597420ffcc027324bb59d04d585e
Lucky Gupta <44520571+theluckygupta@users.noreply.github.com>
2026-05-04 09:44:17 +0000

    fix footer in home layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,12 +1,23 @@
 import '@/lib/globals.css';
 
 import { Navbar } from '@/components/page-ui/Navbar';
+import { Footer } from '@/components/page-ui/Footer';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="dark:bg-black">
+        <div className="flex min-h-screen flex-col dark:bg-black">
+
+            {/* Navbar */}
             <Navbar />
-            {children}
+
+            {/* Page Content */}
+            <main className="flex-1">
+                {children}
+            </main>
+
+            {/* Footer */}
+            <Footer />
+
         </div>
     );
 }
