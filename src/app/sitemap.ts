5685f61f41018a0ca4788dc0c15633431caeb2f9
Lucky Gupta <44520571+theluckygupta@users.noreply.github.com>
2026-05-04 07:28:52 +0000

    force sitemap fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,14 +1,10 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://luckygupta.in'
-
   return [
     {
-      url: `${baseUrl}/`,
+      url: 'https://luckygupta.in',
       lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 1,
     },
   ]
 }
